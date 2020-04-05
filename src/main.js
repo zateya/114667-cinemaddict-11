af8fdbb23c7a184511a308460bb039d74ed8d935
Olga Koval <ok@telemark-it.ru>
2020-04-05 17:29:27 +0300

    fix quotes in main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import {createHeaderProfileTemplate} from "./components/header-profile.js";
-import {createSiteMenuTemplate} from "./components/site-menu.js";
-import {createSortTemplate} from "./components/sorting.js";
-import {createFilmsTemplate} from "./components/films.js";
-import {createFooterStatisticsTemplate} from "./components/footer-statistics.js";
-import {createFilmDetailsTemplate} from "./components/film-details.js";
-import {render} from "./components/utils.js";
+import {createHeaderProfileTemplate} from './components/header-profile.js';
+import {createSiteMenuTemplate} from './components/site-menu.js';
+import {createSortTemplate} from './components/sorting.js';
+import {createFilmsTemplate} from './components/films.js';
+import {createFooterStatisticsTemplate} from './components/footer-statistics.js';
+import {createFilmDetailsTemplate} from './components/film-details.js';
+import {render} from './components/utils.js';
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
